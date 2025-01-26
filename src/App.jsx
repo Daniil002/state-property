4be57faf92b5354f082fdbbb10c78b852b5a918e
Daniil <prokopenkodaniil789@gmail.com>
2025-01-26 19:23:53 +0300

    add reset button

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
-import { useState } from 'react';
-import './App.css';
-import './index.css'
-import Button from './components/Button';
-import Counter from './components/Countner';
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
+import Button from "./components/Button";
+import Counter from "./components/Countner";
 
-
-const text = [
-  "Click me", 
-  "Click me please",
-  "Hit me",
-  "Press me",
-]
+const text = ["Click me", "Click me please", "Hit me", "Press me"];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const incrementCount = () => {
-    setCount(count + 1)
-  }
+    setCount(count + 1);
+  };
+
+  const resetCount = () => {
+    setCount(0);
+  };
+
+  const buttonStyle = { backgroundColor: "lightgreen" };
   return (
     <div className="App">
       <Counter count={count} />
-      {text.map((text, index) => {return <Button onClick={incrementCount} text={text} key={index} />})}
+      {text.map((text, index) => {
+        return <Button onClick={incrementCount} text={text} key={index} />;
+      })}
+      {count > 0 && (
+        <div>
+          <button style={buttonStyle} onClick={resetCount}>
+            Reset
+          </button>
+        </div>
+      )}
     </div>
   );
 }
